ac954d3a283b5e3dff74d4c689acbced5db92a11
ALMLAMBO <36967653+almlambo@users.noreply.github.com>
2019-05-31 10:59:27 +0300

    Add check for free reservation for selected room and query for adding it to the database

--- final/reservation.php
+++ final/reservation.php
@@ -15,9 +15,34 @@
     }
 
     if($statement = $db_conn ->
-        prepare('select BeginingDate, EndDate from reservations where RoomId = ?')) {
+        prepare('select BeginingDate, EndDate from reservations where RoomId = ? and BeginingDate = :bdate and EndDate = :edate')) {
 
+        $statement -> bind_param("i", $_POST["RoomId"]);
+        $statement -> bind_param(":bdate", $_POST["BeginingDate"]);
+        $statement -> bind_param(":edate", $_POST["EndDate"]);
+        $statement -> execute();
+        $statement -> store_result();
+        if($statement -> num_rows > 0) {
+            echo "The reservation for selected period of time and room is not available";
+        }
+        else {
+            if($statement = $db_conn -> prepare('insert into reservations value(?, ?, ?)')) {
+                $bdate = $_POST["BeginingDate"];
+                $edate = $_POST["EndDate"];
+                $roomId = $_POST["RoomId"];
 
+                $statement -> bind_param("ssi", $bdate, $edate, $roomId);
+                $statement -> execute();
+                echo "Reservation has been made successfully!";
+            }
+            else {
+                echo "Could not prepare statement!";
+            }
+        }
+        $statement -> close();
+    }
+    else {
+        echo "Could not prepare statement!";
     }
 
     $db_conn -> close();
