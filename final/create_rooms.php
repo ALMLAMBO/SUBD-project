57b96f94a996247975f41ce0071c4f1bc5046a77
aleks <aleksm007148@gmail.com>
2019-06-07 16:12:23 +0300

    Add password for connection to the database

--- final/create_rooms.php
+++ final/create_rooms.php
@@ -2,7 +2,7 @@
 session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'password';
 $DATABASE_NAME = 'escape_rooms';
 $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
